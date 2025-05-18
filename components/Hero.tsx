dd57151cd7e45022add5a4c2ede6760df32c44b7
Lukáš Mikula <mikula.luky@gmail.com>
2025-05-18 18:01:37 +0200

    add responsivity to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,21 +8,26 @@ function Hero() {
 	useGSAP(
 		() => {
 			const tl = gsap.timeline();
-			tl.fromTo(
+			// prep
+			tl.set('.name', { opacity: 0, y: -20 }).set('h1', {
+				opacity: 0,
+				y: -20,
+				backgroundPosition: '100% 0%',
+			});
+			// animation
+			tl.to(
 				'.name',
-				{ opacity: 0, y: -20 },
-				{ opacity: 1, y: 0, duration: 0.7 }
+				{ opacity: 1, y: 0, duration: 0.7, autoAlpha: 1 },
+				'>+=0.2'
 			)
-				.fromTo(
+				.to('h1', { opacity: 1, y: 0, duration: 0.7, autoAlpha: 1 })
+				.to(
 					'h1',
-					{ opacity: 0, y: -20 },
-					{ opacity: 1, y: 0, duration: 0.7 },
-					'>'
-				)
-				.fromTo(
-					'h1',
-					{ backgroundPosition: '100% 0%' },
-					{ backgroundPosition: '0% 0%', duration: 2, ease: 'power4' },
+					{
+						backgroundPosition: '0% 0%',
+						duration: 2,
+						ease: 'power4',
+					},
 					'<+=0.4'
 				);
 		},
@@ -30,21 +35,21 @@ function Hero() {
 	);
 
 	return (
-		<div
+		<section
 			ref={heroRef}
 			className='h-screen  relative flex-center col'
 		>
-			<div className='text-center flex col '>
-				<div className='text-xl name tracking-tight mb-4 font-bold'>
+			<div className='text-center  flex col'>
+				<div className='sm:text-xl invisible name tracking-tight mb-0 sm:mb-4 font-bold'>
 					Hello, I&apos;m Lukas
 				</div>
 				<div>
-					<h1 className='text-7xl from-text bg-gradient-to-r text-transparent bg-size-[200%] from-10% to-text to-90% relative tracking-tight bg-clip-text font-bold via-cyan-400'>
-						Your next React developer.
+					<h1 className='sm:text-7xl text-5xl from-text invisible bg-gradient-to-r text-transparent bg-size-[200%] from-10% to-text to-90% relative tracking-tight bg-clip-text font-bold via-cyan-400'>
+						Your next React&nbsp;developer.
 					</h1>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
 export default Hero;
