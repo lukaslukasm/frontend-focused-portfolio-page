8d8bac80aa6a0369d54ad6b556a400b3174ef08b
Lukáš Mikula <mikula.luky@gmail.com>
2025-06-10 14:33:41 +0200

    remake the intro: - adjusted animations - changed the wording - add react logo spinning - abstract the big text into DisplayMsg component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,146 +3,135 @@ import { useGSAP } from '@gsap/react';
 import Image from 'next/image';
 import { useRef } from 'react';
 import gsap from '../utils/registerGsap';
+import DisplayMsg from './DisplayMsg';
+import reactLogo from '../public/react-logo.svg';
 
 function Intro() {
-	const introRef = useRef(null);
+  const introRef = useRef(null);
 
-	useGSAP(
-		() => {
-			gsap.timeline({
-				scrollTrigger: {
-					trigger: introRef.current,
-					start: 'top top',
-					end: '+=1000',
-					pin: true,
-				},
-			});
+  useGSAP(
+    () => {
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: introRef.current,
+          start: 'top top',
+          end: '+=500',
+          pin: true,
+        },
+      });
 
-			gsap
-				.timeline({
-					scrollTrigger: {
-						trigger: '.asterisk',
-						start: 'bottom bottom',
-						toggleActions: 'play none none none',
-					},
-				})
-				.fromTo(
-					'.asterisk',
-					{ rotateZ: 0 },
-					{
-						rotateZ: 2000,
-						duration: 1.7,
-					}
-				)
-				.fromTo(
-					'.question-mark',
-					{ rotateY: 0 },
-					{
-						rotateY: 720,
-						duration: 1.7,
-					},
-					'<'
-				)
-				.fromTo(
-					'.skill',
-					{ opacity: 0, top: 0 },
-					{ opacity: 1, top: '-1rem', duration: 0.5, ease: 'back.out' },
-					'<'
-				)
-				.addLabel('showDashboard')
-				.fromTo(
-					'.line1',
-					{ rotateX: 0, y: 0 },
-					{ rotateX: 90, y: -20, duration: 0.2 },
-					'showDashboard'
-				)
-				.fromTo(
-					'.line2',
-					{ rotateX: -90, y: 0 },
-					{ rotateX: 0, y: -70, duration: 0.2 },
-					'showDashboard'
-				)
-				.fromTo(
-					'.skill',
-					{ left: 0 },
-					{ left: '16rem', duration: 0.2, ease: 'back.out' },
-					'showDashboard'
-				)
-				.fromTo(
-					'.dashboard',
-					{ y: '80vh' },
-					{ y: '60vh', duration: 1.5, ease: 'power4.inOut' },
-					'showDashboard'
-				)
-				.to('.line2', { scale: 0.8, duration: 0.2 }, 'showDashboard+=1')
-				.fromTo(
-					'.skill',
-					{ scale: 1, left: '16rem', top: '-1rem' },
-					{ scale: 0.8, left: '17.6rem', top: '-0.8rem', duration: 0.2 },
-					'<'
-				)
-				.fromTo(
-					'.dashboard',
-					{ y: '60vh', scale: 0.75 },
-					{ y: 0, scale: 1, duration: 0.5, ease: 'power4.out' },
-					'>'
-				)
-				.fromTo(
-					'.heading',
-					{ opacity: 0, y: -20 },
-					{ opacity: 1, y: 0, duration: 0.5, ease: 'power4.out' },
-					'>'
-				);
-		},
-		{ scope: introRef }
-	);
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: '.reactLogo',
+            start: 'bottom bottom',
+            toggleActions: 'play none none none',
+          },
+        })
+        .fromTo(
+          '.reactLogo',
+          { rotateZ: 0 },
+          {
+            rotateZ: 120,
+            duration: 4,
+            ease: 'linear',
+          },
+        )
+        .fromTo(
+          '.question-mark',
+          { rotateY: 0 },
+          {
+            rotateY: 720,
+            duration: 1.7,
+          },
+          '<',
+        )
+        // .fromTo(
+        //   '.line1 .skill',
+        //   { opacity: 0, top: 0 },
+        //   { opacity: 1, top: '-1rem', duration: 0.5, ease: 'back.out' },
+        //   '<',
+        // )
+        .addLabel('showDashboard', '>')
+        .fromTo(
+          '.line1',
+          { rotateX: 0, y: 0 },
+          { rotateX: 90, y: -30, duration: 0.2 },
+          'showDashboard',
+        )
+        // .fromTo(
+        //   '.line1 .skill',
+        //   { rotateX: 0, y: 0 },
+        //   { rotateX: 90, y: -20, duration: 0.2 },
+        //   'showDashboard',
+        // )
+        .fromTo(
+          '.line2',
+          { rotateX: -90, y: 0 },
+          { rotateX: 0, y: -70, duration: 0.2 },
+          'showDashboard',
+        )
+        .to('.intro-wrapper', { scale: 0.8, duration: 0.3 }, 'showDashboard+=1')
+        .to('.intro-wrapper', { y: 30, duration: 0.3 }, '<+=0.05')
+        .fromTo(
+          '.dashboard',
+          { y: '80vh' },
+          { y: 0, duration: 0.7, ease: 'power4.out' },
+          '<+=0.1',
+        )
+        .fromTo(
+          '.dashboard',
+          { scale: 0.75 },
+          { scale: 1, duration: 0.7, ease: 'power4.out' },
+          '<+=0.2',
+        )
+        .fromTo(
+          '.heading',
+          { opacity: 0, y: -20 },
+          { opacity: 1, y: 0, duration: 0.5, ease: 'power4.out' },
+          '>',
+        );
+    },
+    { scope: introRef },
+  );
 
-	return (
-		<>
-			<section
-				ref={introRef}
-				className='h-screen relative w-full overflow-y-hidden flex-center col'
-			>
-				<h2 className='text-6xl heading opacity-0 top-40 left-0 text-text font-bold absolute'>
-					<span className='text-cyan-500 topic-badge block -mb-2 '>
-						Skill
-						<br />
-					</span>
-					Showcase
-				</h2>
-				<div className='text-center flex col relative'>
-					<div className='skill topic-badge origin-bottom-right opacity-0 mb-4 absolute left-0 top-0 text-cyan-500'>
-						Skill
-					</div>
-					<div className='text-7xl line1 text-text relative tracking-tighter font-bold'>
-						Wanna see some cool sh
-						<div className='transform-center asterisk inline-block'>
-							<Image
-								alt='*'
-								className=' m-1'
-								width={42}
-								height={42}
-								src='/asterisk.svg'
-							/>
-						</div>
-						t<span className='inline-block question-mark'>?</span>
-					</div>
-					<div className='text-7xl line2 text-text  rotate-x-90 -translate-y-full relative tracking-tighter font-bold'>
-						Watch this
-					</div>
-				</div>
-				<div className='aspect-video overflow-hidden absolute scale-75 translate-y-[80vh] dashboard w-full border-3 border-cyan-300 flex-center rounded-2xl placeholder-elm'>
-					<Image
-						alt='dashboard'
-						className='w-full h-full object-cover'
-						width={1920}
-						height={1080}
-						src='/dashboard-placeholder.png'
-					/>
-				</div>
-			</section>
-			<div className='h-[60vh]'></div>
-		</>
-	);
+  return (
+    <>
+      <section
+        ref={introRef}
+        className="flex-center relative flex h-screen w-full overflow-y-hidden"
+      >
+        <DisplayMsg
+          category="Skill"
+          className="heading absolute top-20 left-4 opacity-0 sm:top-40"
+        >
+          Showcase
+        </DisplayMsg>
+        <div className="intro-wrapper col flex-center relative h-full flex-grow">
+          <Image
+            src={reactLogo}
+            alt="react logo"
+            className="reactLogo mb-5 size-20 opacity-70 sm:mb-10 sm:size-32"
+          />
+          <DisplayMsg className="line1 flex">
+            <div className="col relative inline-block text-center">
+              Wanna see something cool
+              <span className="question-mark inline-block">?</span>
+            </div>
+          </DisplayMsg>
+          <DisplayMsg className="line2 rotate-x-90">Watch this</DisplayMsg>
+        </div>
+        <div className="dashboard border-primary/10 flex-center placeholder-elm absolute aspect-video h-full max-h-[70vh] max-w-[calc(100%-2rem)] translate-y-[80vh] scale-75 overflow-hidden rounded-2xl drop-shadow-xl max-sm:mt-20 sm:max-h-[50vh] sm:drop-shadow-2xl">
+          <Image
+            alt="dashboard"
+            className="h-full w-full object-cover"
+            fill
+            src="/dashboard-placeholder.png"
+          />
+        </div>
+      </section>
+      <div className="h-[40vh] max-sm:h-[90vh]"></div>
+    </>
+  );
 }
 export default Intro;
