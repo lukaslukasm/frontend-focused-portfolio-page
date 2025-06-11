70537a92e94563b0b811c049271305c1ea29ec43
Lukáš Mikula <mikula.luky@gmail.com>
2025-06-11 12:49:37 +0200

    add more features to carousel: - arrow styles - responsivity - arrow disabling based on scroll position

--- components/ShowcaseFeatures.tsx
+++ components/ShowcaseFeatures.tsx
@@ -2,10 +2,14 @@ import '@splidejs/react-splide/css';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import DisplayMsg from './DisplayMsg';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function ShowcaseFeatures() {
   const scrollRef = useRef<HTMLDivElement>(null);
+  const [disabledArrow, setDisabledArrow] = useState<'prev' | 'next' | null>(
+    'prev',
+  );
+
   useEffect(() => {
     const el = scrollRef.current;
     if (!el) return;
@@ -22,8 +26,9 @@ function ShowcaseFeatures() {
     };
 
     const onMouseMove = (e: MouseEvent) => {
-      if (!isDown) return;
+      if (!isDown || !el) return;
       e.preventDefault();
+
       const x = e.pageX - el.offsetLeft;
       const walk = (x - startX) * 1.5; // scroll speed
       el.scrollLeft = scrollLeft - walk;
@@ -33,6 +38,20 @@ function ShowcaseFeatures() {
       isDown = false;
       el.classList.remove('cursor-grabbing');
     };
+
+    const handleScroll = () => {
+      const maxScrollLeft = el.scrollWidth - el.clientWidth;
+
+      if (el.scrollLeft <= 0) {
+        setDisabledArrow('prev');
+      } else if (el.scrollLeft >= maxScrollLeft - 1) {
+        setDisabledArrow('next');
+      } else {
+        setDisabledArrow(null);
+      }
+    };
+
+    el.addEventListener('scroll', handleScroll);
 
     el.addEventListener('mousedown', onMouseDown);
     el.addEventListener('mousemove', onMouseMove);
@@ -44,6 +63,7 @@ function ShowcaseFeatures() {
       el.removeEventListener('mousemove', onMouseMove);
       el.removeEventListener('mouseup', onMouseUp);
       el.removeEventListener('mouseleave', onMouseUp);
+      el.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -65,10 +85,10 @@ function ShowcaseFeatures() {
       <DisplayMsg className="mt-3 mb-12" category="Skill">
         <h2>Showcase - features</h2>
       </DisplayMsg>
-      <div className="h-[50rem]">
+      <div className="h-[43rem]">
         <div
           ref={scrollRef}
-          className="no-scrollbar absolute right-0 left-0 flex snap-x snap-mandatory scroll-px-4 overflow-x-scroll scroll-smooth py-20 sm:left-0 sm:scroll-px-[var(--responsive-gutter-width)]"
+          className="no-scrollbar absolute right-0 left-0 flex snap-x snap-mandatory scroll-px-4 overflow-x-scroll scroll-smooth py-5 sm:left-0 sm:scroll-px-[var(--responsive-gutter-width)]"
         >
           <ul className="flex w-max gap-8 px-4 sm:px-[var(--responsive-gutter-width)]">
             <li className="h-[40rem] min-w-88 snap-start rounded-3xl bg-white drop-shadow-lg"></li>
@@ -80,32 +100,31 @@ function ShowcaseFeatures() {
           </ul>
         </div>
       </div>
-      <div className="mt-8 flex justify-center gap-4">
+      <div className="mt-8 flex justify-center gap-4 self-end">
         <button
           onClick={() => scrollByCard('left')}
-          className="rounded-full bg-gray-200 p-2 hover:bg-gray-300"
+          disabled={disabledArrow === 'prev'}
+          className="rounded-full bg-gray-200 p-2 transition-all hover:bg-gray-300 disabled:opacity-50"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-            <path d="m20 25c-.3838 0-.7676-.1465-1.0605-.4395l-5.5-5.5c-.5859-.5854-.5859-1.5356 0-2.1211l5.5-5.5c.5859-.5859 1.5352-.5859 2.1211 0 .5859.5854.5859 1.5356 0 2.1211l-4.4395 4.4395 4.4395 4.4395c.5859.5854.5859 1.5356 0 2.1211-.293.293-.6768.4395-1.0605.4395z"></path>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 -960 960 960"
+            className="fill-text size-7 scale-110"
+          >
+            <path d="m471-480 137 137q20 19 20 46.98 0 27.98-20 48.02-19 19-46.98 19-27.98 0-48.02-19L330-432q-9.91-9.71-15.45-22.21Q309-466.71 309-480q0-13.57 5.55-26.32Q320.09-519.07 330-529l183-184q20.04-19 48.02-19Q589-732 608-713q20 20.04 20 48.02Q628-637 608-618L471-480Z" />
           </svg>
         </button>
         <button
           onClick={() => scrollByCard('right')}
-          className="rounded-full bg-gray-200 p-2 hover:bg-gray-300"
+          disabled={disabledArrow === 'next'}
+          className="rounded-full bg-gray-200 p-2 transition-all hover:bg-gray-300 disabled:opacity-50"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+            viewBox="0 -960 960 960"
+            className="fill-text size-7 scale-110 rotate-z-180"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            />
+            <path d="m471-480 137 137q20 19 20 46.98 0 27.98-20 48.02-19 19-46.98 19-27.98 0-48.02-19L330-432q-9.91-9.71-15.45-22.21Q309-466.71 309-480q0-13.57 5.55-26.32Q320.09-519.07 330-529l183-184q20.04-19 48.02-19Q589-732 608-713q20 20.04 20 48.02Q628-637 608-618L471-480Z" />
           </svg>
         </button>
       </div>
