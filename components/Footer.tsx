571214ab2d9502da006aa8dac2612128bc7c5266
Lukáš Mikula <mikula.luky@gmail.com>
2025-06-13 14:57:27 +0200

    small visual changes to placeholding elms: - add WIP text to the portfolio page - shirk the footer, adjust styling of the text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div
       id="contact"
-      className="flex-center bg-text relative h-screen w-screen text-3xl font-black text-cyan-50 uppercase"
+      className="flex-center bg-text relative h-60 w-screen text-3xl font-black text-cyan-50/50"
     >
       Footer
     </div>
