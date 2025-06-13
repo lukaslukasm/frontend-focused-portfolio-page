d49ddb155d20aacb3122a1edd9682e276297222b
Lukáš Mikula <mikula.luky@gmail.com>
2025-06-13 14:56:12 +0200

    done: - a11y - mobile menu - desktop menu - designs - mobile menu toggle - r10y

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/utils/cn';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -9,9 +9,8 @@ import { Squeeze as Hamburger } from 'hamburger-react';
 function Header() {
   const [hideNav, setHideNav] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  const openMenuAnimation = gsap.timeline({ duration: 1, ease: 'power4.out' });
-  openMenuAnimation.to('.mobile-nav-wrapper', { top: 0, opacity: 1 });
-  openMenuAnimation.pause();
+  const openMenuAnimation = useRef<gsap.core.Timeline | null>(null);
+  const mobileNavFirstLink = useRef<HTMLAnchorElement>(null);
 
   useGSAP(() => {
     gsap.registerPlugin(ScrollToPlugin);
@@ -21,6 +20,20 @@ function Header() {
       { rotateZ: 0 },
       { rotateZ: 360, ease: 'linear', repeat: -1, duration: 5 },
     );
+
+    openMenuAnimation.current = gsap
+      .timeline({ paused: true, ease: 'power4.out' })
+      .fromTo(
+        '.mobile-nav-wrapper',
+        { y: '-100%', opacity: 0.5 },
+        { y: 0, opacity: 1 },
+      )
+      .fromTo(
+        '.mobile-nav-wrapper ul li',
+        { opacity: 0, y: -20 },
+        { opacity: 1, y: 0, stagger: 0.1 },
+        '<+=0.4',
+      );
   });
 
   useEffect(() => {
@@ -49,70 +62,200 @@ function Header() {
     };
 
     window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
   }, []);
 
   useEffect(() => {
-    if (menuOpen) openMenuAnimation.play();
-    else openMenuAnimation.play(-1);
+    if (menuOpen) {
+      openMenuAnimation.current?.play();
+      // document.body.classList.add('!h-screen', 'overflow-y-hidden');
+      mobileNavFirstLink.current?.focus();
+    } else {
+      openMenuAnimation.current?.reverse();
+      // document.body.classList.remove('!h-screen', 'overflow-y-hidden');
+    }
   }, [menuOpen]);
 
   return (
-    <div
+    <nav
       className={cn(
-        'fixed top-0 z-50 h-20 w-full px-4 py-4 text-xl font-black transition-transform [&_a,button]:opacity-50 [&_a,button]:transition-opacity [&_a:hover,button:hover]:opacity-100 [&_button]:cursor-pointer',
-        hideNav && '-translate-y-full',
+        'fixed inset-0 top-0 z-50 h-12 text-xl font-black transition-transform max-sm:m-2 sm:h-20 sm:px-4 sm:py-4 [&_a,button]:opacity-50 [&_a,button]:transition-opacity [&_a:hover,button:hover]:opacity-100 [&_button]:cursor-pointer',
+        hideNav && !menuOpen && '-translate-y-20',
       )}
     >
-      <div className="mx-auto flex max-w-[100rem] items-center justify-between">
-        {/* logo */}
-        <Link href="/" aria-label="Go to Homepage">
-          Home
-        </Link>
-        {/* mobile navbar */}
-        <div className="flex-center justify-between gap-4 sm:hidden">
-          <Hamburger
-            toggle={setMenuOpen}
-            color="oklch(0.446 0.03 256.802)"
-            rounded
-            toggled={menuOpen}
-          />
-        </div>
+      <div className="mx-auto flex items-center justify-end transition-transform sm:justify-between">
         {/* desktop navbar */}
-        <nav className="flex gap-8 max-sm:hidden">
-          <Link
-            aria-label="Go to portfolio page"
-            href="/portfolio"
-            className="flex items-center"
-          >
-            Portfolio
-          </Link>
-          <button
-            type="button"
-            aria-label="Scroll to about section"
-            onClick={() => gsap.to(window, { duration: 1, scrollTo: '#about' })}
-          >
-            About
-          </button>
-          <button
-            type="button"
-            aria-label="Scroll to contact section"
-            onClick={() =>
-              gsap.to(window, { duration: 1, scrollTo: '#contact' })
-            }
-            className="bg-text relative isolate overflow-hidden rounded-full !opacity-100 hover:[&_.blue-bg]:opacity-100"
-          >
-            <div className="blue-bg bg-primary absolute inset-0 opacity-0 transition-opacity"></div>
-            <div className="gradient-border from-primary to-primary absolute top-1/2 left-1/2 h-40 w-40 -translate-x-1/2 -translate-y-1/2 bg-conic-180 via-transparent blur-md"></div>
-            <div className="relative z-10 rounded-full px-5 py-3 leading-none font-bold text-white">
-              Contact
-            </div>
-          </button>
-        </nav>
+        <ul className="flex w-full gap-8 max-sm:hidden [&_li]:flex [&_li]:items-center">
+          {/* logo */}
+          <li>
+            <Link href="/" aria-label="Go to Homepagea" className="">
+              Home
+            </Link>
+          </li>
+          <div className="flex-grow" aria-hidden></div>
+          <li>
+            <Link
+              aria-label="Go to portfolio page"
+              href="/portfolio"
+              className="flex items-center"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <button
+              type="button"
+              aria-label="Scroll to about section"
+              onClick={() =>
+                gsap.to(window, { duration: 1, scrollTo: '#about' })
+              }
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <button
+              type="button"
+              aria-label="Scroll to contact section"
+              onClick={() =>
+                gsap.to(window, { duration: 1, scrollTo: '#contact' })
+              }
+              className="bg-text relative isolate overflow-hidden rounded-full !opacity-100 hover:[&_.blue-bg]:opacity-100"
+            >
+              <div className="blue-bg bg-primary absolute inset-0 opacity-0 transition-opacity"></div>
+              <div className="gradient-border from-primary to-primary absolute top-1/2 left-1/2 h-40 w-40 -translate-x-1/2 -translate-y-1/2 bg-conic-180 via-transparent blur-md"></div>
+              <div className="relative z-10 rounded-full px-5 py-3 leading-none font-bold text-white">
+                Contact
+              </div>
+            </button>
+          </li>
+        </ul>
         {/* mobile nav menu */}
-        <div className="mobile-nav-wrapper fixed inset-0 z-50 -translate-y-full bg-gray-200 opacity-0"></div>
+        <div
+          role="dialog"
+          aria-modal="true"
+          aria-hidden={!menuOpen}
+          inert={!menuOpen}
+          className={cn(
+            'mobile-nav-wrapper',
+            'fixed',
+            'inset-0',
+            'z-40',
+            'h-screen',
+            '-translate-y-full',
+            'bg-gray-100',
+            'opacity-50',
+          )}
+        >
+          <ul className="col mt-20 ml-8 flex gap-4">
+            <li>
+              <Link
+                ref={mobileNavFirstLink}
+                href="/"
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                aria-label="Go to Homepage"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                aria-label="Go to portfolio page"
+                href="/portfolio"
+                onClick={() => {
+                  setMenuOpen(false);
+                }}
+                className="flex w-max items-center"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <button
+                type="button"
+                aria-label="Scroll to about section"
+                onClick={() => {
+                  setMenuOpen(false);
+                  gsap.to(window, { duration: 1, scrollTo: '#about' });
+                }}
+              >
+                About
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                aria-label="Scroll to contact section"
+                onClick={() => {
+                  setMenuOpen(false);
+                  gsap.to(window, { duration: 1, scrollTo: '#contact' });
+                }}
+              >
+                Contact
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-center gap-2">
+          <div
+            aria-hidden={menuOpen}
+            inert={menuOpen}
+            className={cn(
+              'z-50',
+              'rounded-full',
+              'border',
+              'border-white/30',
+              'bg-gray-300/30',
+              'text-base',
+              'px-3',
+              'scale-90',
+              'py-1',
+              'transition-opacity',
+              'backdrop-blur-md',
+              'sm:hidden',
+              menuOpen && 'opacity-0',
+            )}
+          >
+            <button
+              type="button"
+              aria-label="Scroll to contact section"
+              className="text-text mb-1.5 text-sm leading-none font-bold !opacity-90"
+              onClick={() =>
+                gsap.to(window, { duration: 1, scrollTo: '#contact' })
+              }
+            >
+              Contact
+            </button>
+          </div>
+          {/* mobile navbar toggle */}
+          <div
+            className={cn(
+              'z-50',
+              'rounded-full',
+              'border',
+              'border-white/30',
+              'bg-gray-300/30',
+              'backdrop-blur-md',
+              'sm:hidden',
+              menuOpen && 'border-transparent bg-transparent',
+            )}
+          >
+            <Hamburger
+              toggle={setMenuOpen}
+              color="oklch(44.6% 0.03 256.802)"
+              rounded
+              tab-index={0}
+              size={20}
+              toggled={menuOpen}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 export default Header;
