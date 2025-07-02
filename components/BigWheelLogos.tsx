255db1f295275c145b4d959ba4e1e2550cb6bcaf
Lukáš Mikula <mikula.luky@gmail.com>
2025-07-02 20:01:18 +0200

    make `Experience` section component more readable; extract wheels of logos into separate components

---
+++ components/BigWheelLogos.tsx
@@ -0,0 +1,88 @@
+import Image from 'next/image';
+import next from '../public/logos/nextjs.png';
+import shopify from '../public/logos/shopify.png';
+import filament from '../public/logos/filament.png';
+import bootstrap from '../public/logos/bootstrap.png';
+import alpine from '../public/logos/alpine.png';
+import builderio from '../public/logos/builderio.svg';
+import drupal from '../public/logos/drupal.png';
+import email from '../public/logos/email.png';
+import framer from '../public/logos/framer.png';
+import gsapLogo from '../public/logos/gsap.svg';
+import hubspot from '../public/logos/hubspot.webp';
+import jquery from '../public/logos/jquery.svg';
+import phpslim from '../public/logos/php-slim.jpg';
+import woo from '../public/logos/woo.png';
+import wordpress from '../public/logos/wordpress.png';
+import docker from '../public/logos/docker.webp';
+import LogosWheel from './LogosWheel';
+
+/**
+ * Renders a bigger circle of technologies' logos using `LogosWheel` component.
+ * Inclues animation, styling, a11y, responsiveness.
+ *
+ */
+function BigWheelLogos() {
+  return (
+    <div className="absolute inset-0">
+      <LogosWheel
+        radiusWidthMobile={160}
+        radiusWidthDesktop={340}
+        spinDirection="left"
+      >
+        <div className="card fav size-12 sm:size-24">
+          <Image src={next} alt="Next.js logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={bootstrap} alt="Bootstrap logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={builderio} alt="Builder.io logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={docker} alt="Docker logo" />
+        </div>
+        <div className="card fav size-12 sm:size-24">
+          <Image src={filament} alt="Filament logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={wordpress} alt="Wordpress logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={alpine} alt="Alpine logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={phpslim} alt="PHP SLim logo" />
+        </div>
+        <div className="card fav size-12 sm:size-24">
+          <Image src={shopify} alt="Shopify logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={jquery} alt="JQuery logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={framer} alt="Bootstrap logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={hubspot} alt="Hubspot logo" />
+        </div>
+        <div className="card fav size-12 sm:size-24">
+          <Image src={gsapLogo} alt="GSAP logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={drupal} alt="Drupal logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image src={woo} alt="Woocommerce logo" />
+        </div>
+        <div className="card size-12 sm:size-24">
+          <Image
+            src={email}
+            alt="Icon illustrating Coding Custom email templates"
+          />
+        </div>
+      </LogosWheel>
+    </div>
+  );
+}
+export default BigWheelLogos;
---
+++ components/SmallWheelLogos.tsx
@@ -0,0 +1,50 @@
+import LogosWheel from './LogosWheel';
+import Image from 'next/image';
+import reactLogo from '../public/logos/react-logo.svg';
+import tw from '../public/logos/tw.png';
+import laravel from '../public/logos/laravel.png';
+import shoptet from '../public/logos/shoptet.png';
+import silverstripe from '../public/logos/silverstripe.png';
+import sas360 from '../public/logos/sas360.png';
+import strapi from '../public/logos/strapi.png';
+import node from '../public/logos/node.png';
+import 'react-tooltip/dist/react-tooltip.css';
+
+/**
+ * Renders a smaller circle of technologies' logos using `LogosWheel` component.
+ * Inclues animation, styling, a11y, responsiveness.
+ *
+ */
+function SmallWheelLogos() {
+  return (
+    <div className="">
+      <LogosWheel radiusWidthMobile={70} className="" radiusWidthDesktop={180}>
+        <div className="card fav size-8 sm:size-18">
+          <Image src={reactLogo} alt="React logo" />
+        </div>
+        <div className="card size-8 sm:size-18">
+          <Image src={silverstripe} alt="Silverstripe logo" />
+        </div>
+        <div className="card fav size-8 sm:size-18">
+          <Image src={strapi} alt="Strapi logo" />
+        </div>
+        <div className="card size-8 sm:size-18">
+          <Image src={shoptet} alt="Shoptet logo" />
+        </div>
+        <div className="card fav size-8 sm:size-18">
+          <Image src={tw} alt="Tailwind logo" />
+        </div>
+        <div className="card size-8 sm:size-18">
+          <Image src={laravel} alt="Laravel logo" />
+        </div>
+        <div className="card fav size-8 sm:size-18">
+          <Image src={node} alt="Node.js logo" />
+        </div>
+        <div className="card size-8 sm:size-18">
+          <Image src={sas360} alt="SAS360 logo" />
+        </div>
+      </LogosWheel>
+    </div>
+  );
+}
+export default SmallWheelLogos;
