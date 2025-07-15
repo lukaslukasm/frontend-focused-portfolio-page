5f9e65b10fade4c27911fc3cfe2e4ed537e43cb8
Lukáš Mikula <mikula.luky@gmail.com>
2025-07-15 20:59:39 +0200

    add content: - remove placeholder slides - add posters for 3 projects' slides with graphic and final texting. expand content still missing

--- components/OtherProjects.tsx
+++ components/OtherProjects.tsx
@@ -1,7 +1,6 @@
 import '@splidejs/react-splide/css';
 import DisplayMsg from './DisplayMsg';
 import Carousel from './Carousel';
-import CarouselSlide from './CarouselSlide';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { useRef } from 'react';
@@ -47,7 +46,7 @@ function OtherProjects() {
       <DisplayMsg
         className="heading mb-4 ml-[var(--responsive-gutter-width)] translate-y-8 opacity-0"
         category="Selected Work"
-        categoryClassName="text-green-500"
+        categoryClassName="text-green-600"
       >
         <h2>Other Projects</h2>
       </DisplayMsg>
@@ -65,19 +64,61 @@ function OtherProjects() {
           }
           btnClassName="bg-green-600 text-bg/80"
         >
-          <Image
-            src="/images/wordel-poster.png"
-            alt=""
-            width={360}
-            height={600}
-            className="absolute inset-0 -z-10 object-cover"
-          />
+          <div className="absolute inset-0 -right-10 -z-10 size-full overflow-hidden rounded-3xl">
+            <Image
+              src="/images/reservation-system-poster.png"
+              alt=""
+              width={460}
+              height={700}
+              className="scale-110 object-cover blur-xs"
+            />
+          </div>
+          <div className="col flex">
+            <DisplayMsg
+              category="Engineered for zero friction"
+              categoryClassName="text-green-600"
+            >
+              <h3 className="text-text text-left text-[1.4rem] sm:text-[1.8rem]">
+                Reservation System
+              </h3>
+            </DisplayMsg>
+            <p className="text-text mt-2 text-sm opacity-70 sm:text-base">
+              A simple reservation system made using the finest frontend
+              practices used by the industry.
+            </p>
+          </div>
+        </ExpandableCarouselSlide>
+        <ExpandableCarouselSlide
+          expandedContent={
+            <div className="col flex">
+              <VisuallyHidden>
+                <Dialog.Title>Klenotnicky Radca</Dialog.Title>
+                <Dialog.Description></Dialog.Description>
+              </VisuallyHidden>
+              <h1>this is the content of expanded carousel slide</h1>
+              <button>button</button>
+            </div>
+          }
+          btnClassName="bg-green-600 text-bg/80"
+        >
+          <div className="absolute top-0 bottom-0 left-0 -z-10 size-full overflow-hidden rounded-3xl">
+            <Image
+              src="/images/amawell-poster.png"
+              alt="Jewerly Wizard poster"
+              fill
+              className="scale-105 object-cover blur-xs"
+            />
+          </div>
           <DisplayMsg
-            category="Going strong for 3yrs 💪"
-            categoryClassName="text-green-700"
+            category="Advanced gestures"
+            categoryClassName="text-green-600"
           >
-            <h3 className="text-bg text-left">Wordel</h3>
+            <h3 className="text-text text-left">Jewerly Wizard</h3>
           </DisplayMsg>
+          <p className="text-text mt-2 text-sm opacity-70 sm:text-base">
+            A multi-step, input-driven branching wizard with navigation and
+            granular gestures handling.
+          </p>
         </ExpandableCarouselSlide>
         <ExpandableCarouselSlide
           expandedContent={
@@ -86,73 +127,34 @@ function OtherProjects() {
                 <Dialog.Title>Hello world</Dialog.Title>
                 <Dialog.Description>Hello world</Dialog.Description>
               </VisuallyHidden>
-              <h1>the second slide expandable</h1>
+              <h1>this is the content of expanded carousel slide</h1>
               <button>button</button>
             </div>
           }
           btnClassName="bg-green-600 text-bg/80"
         >
-          <Image
-            src="/images/wordel-poster.png"
-            alt=""
-            width={360}
-            height={600}
-            className="absolute inset-0 -z-10 object-cover"
-          />
-          <DisplayMsg
-            category="Going strong for 3yrs 💪"
-            categoryClassName="text-green-700"
-          >
-            <h3 className="text-bg text-left">Wordel</h3>
-          </DisplayMsg>
+          <div className="absolute inset-0 -right-10 -z-10 size-full overflow-hidden rounded-3xl">
+            <Image
+              src="/images/wordel-poster.png"
+              alt=""
+              width={460}
+              height={700}
+              className="scale-110 object-cover blur-xs brightness-50"
+            />
+          </div>
+          <div className="col flex">
+            <DisplayMsg
+              category="Beware! Highly addictive!"
+              categoryClassName="text-green-600"
+            >
+              <h3 className="text-bg text-left">Wordeľ</h3>
+            </DisplayMsg>
+            <p className="mt-2 text-sm text-white opacity-70 sm:text-base">
+              Visually polished version of the Wordle game with added support
+              for Slovak language.
+            </p>
+          </div>
         </ExpandableCarouselSlide>
-        <CarouselSlide className="bg-yellow-300">
-          <DisplayMsg
-            category="Entertainment"
-            categoryClassName=" text-white/50"
-          >
-            <h3 className="text-white">Traumatised kids</h3>
-          </DisplayMsg>
-          <div className="flex-grow"></div>
-          <img
-            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/afafe003-d161-4420-b1f0-57deb451cc3c/d735xq8-c61f2304-d3dc-460f-80fc-413be8f3fe10.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FmYWZlMDAzLWQxNjEtNDQyMC1iMWYwLTU3ZGViNDUxY2MzY1wvZDczNXhxOC1jNjFmMjMwNC1kM2RjLTQ2MGYtODBmYy00MTNiZThmM2ZlMTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.NZ-6qMMJ-882FkNtjicibaSDdweXwDWkb6Uwjo8URgA"
-            alt=""
-          />
-        </CarouselSlide>
-        <CarouselSlide className="isolate justify-end">
-          <img
-            src="https://www.petrolissimo.sk/www/files/news/23/detail_porsche-911-gt3-992-13_132506a4c.jpg"
-            alt=""
-            className="absolute inset-0 h-full object-cover"
-          />
-          <DisplayMsg
-            category="Precision"
-            className="relative z-10"
-            categoryClassName=" text-white/50"
-          >
-            <h3 className="text-white">Reach Top Speeds</h3>
-          </DisplayMsg>
-        </CarouselSlide>
-        <CarouselSlide>
-          <DisplayMsg
-            category="Security"
-            className="relative z-10"
-            categoryClassName="text-emerald-600/50"
-          >
-            <h3>Maximum Safety</h3>
-          </DisplayMsg>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
-            incidunt, autem nesciunt tempore sint amet vero libero et illo
-            blanditiis?
-          </p>
-          <div className="flex-grow"></div>
-          <img
-            src="https://www.pngplay.com/wp-content/uploads/8/Padlock-PNG-Clipart-Background.png"
-            alt=""
-            className="w-4/5"
-          />
-        </CarouselSlide>
       </Carousel>
     </section>
   );
