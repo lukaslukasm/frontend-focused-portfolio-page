a004ded3354c5693882d672430f5c1460d9e4abb
Lukáš Mikula <mikula.luky@gmail.com>
2025-07-25 18:22:42 +0200

    animations pinned for desktop

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -20,6 +20,22 @@ function Experience() {
     () => {
       const mm = gsap.matchMedia();
 
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: '.experience > .heading',
+            pin: true,
+            start: 'bottom top+=200',
+            end: '+=2000',
+          },
+        })
+        .to('.heading', {
+          y: 0,
+          duration: 0.75,
+          opacity: 1,
+          stagger: 0.2,
+        });
+
       mm.add('(max-width:640px)', () => {
         gsap
           .timeline({
@@ -79,35 +95,25 @@ function Experience() {
         gsap
           .timeline({
             scrollTrigger: {
-              trigger: experienceRef.current,
-              start: 'center-=200 bottom',
+              trigger: '.first-subsection',
+              start: 'top top+=200',
+              pin: true,
+              scrub: 1,
+              end: '+=2000',
             },
-          })
-          .to('.heading', {
-            y: 0,
-            duration: 0.75,
-            opacity: 1,
-            stagger: 0.2,
           })
           .to('.line1', {
             opacity: 1,
             y: 0,
-            rotateX: 0,
-            delay: 1,
             ease: 'power4.out',
           })
-          .to(
-            '.line1',
-            { opacity: 0, y: -36, ease: 'power4.out', rotateX: -80 },
-            '>+=3',
-          )
+          .to('.line1', { opacity: 0, x: -200, filter: 'blur(20px)' }, '>+=0.5')
           .to(
             '.line2',
             {
               opacity: 1,
-              y: 0,
-              rotateX: 0,
-              ease: 'power4.out',
+              x: 0,
+              filter: 'blur(0px)',
             },
             '<',
           )
@@ -127,7 +133,8 @@ function Experience() {
               filter: 'grayscale(0)',
             },
             '<',
-          );
+          )
+          .fromTo('.heading', { opacity: 1 }, { opacity: 1, duration: 0.5 });
       });
     },
     { scope: experienceRef },
@@ -137,7 +144,7 @@ function Experience() {
     <section
       id="experience"
       ref={experienceRef}
-      className="experience relative h-auto w-full max-w-full gap-0 overflow-x-visible max-sm:px-0"
+      className="experience relative w-full max-w-full gap-0 overflow-x-visible max-sm:px-0"
     >
       <DisplayMsg
         className="heading z-50 flex translate-y-8 opacity-0 max-sm:ml-[var(--responsive-gutter-width)]"
@@ -146,26 +153,28 @@ function Experience() {
       >
         <h2>Technologies</h2>
       </DisplayMsg>
-      <div className="relative min-h-[95svh] w-full sm:min-h-[70vh]">
-        <div className="col sm:absolute-center z-10 flex w-full max-sm:absolute max-sm:bottom-0">
-          <DisplayMsg className="line1 translate-y-10 -rotate-x-45 opacity-0">
-            <p className="bg-linear-to-t from-transparent via-cyan-50 to-transparent text-5xl leading-[1.2] font-bold max-sm:px-[var(--responsive-gutter-width)] max-sm:text-3xl sm:py-32 xl:text-center">
-              Over the past few years, I&apos;ve worked <br /> with a wide range
-              of technologies
-            </p>
-          </DisplayMsg>
-          <DisplayMsg className="line2 absolute translate-y-8 opacity-0 max-sm:px-[var(--responsive-gutter-width)] lg:top-1/4 lg:max-w-96">
-            <p className="text-text bg-linear-to-t from-transparent via-cyan-50 to-transparent text-5xl leading-[1.2] max-sm:text-3xl sm:max-lg:py-32 sm:max-lg:text-center lg:text-4xl">
-              <span className="text-highlight">
-                Some of them truly stuck with me,
-              </span>{' '}
-              so those are the ones I&apos;m taking along.
-            </p>
-          </DisplayMsg>
-        </div>
-        <div className="wheels sm:absolute-center relative col-span-2 row-span-2 w-[min(100%,43rem)] overflow-hidden max-sm:-mt-16">
-          <SmallWheelLogos />
-          <BigWheelLogos />
+      <div className="first-subsection-pin">
+        <div className="first-subsection relative min-h-[95svh] w-full sm:min-h-[70vh]">
+          <div className="col sm:absolute-center z-10 flex w-full max-sm:absolute max-sm:bottom-0">
+            <DisplayMsg className="line1 translate-y-10 opacity-0">
+              <p className="bg-linear-to-t from-transparent via-cyan-50 to-transparent text-5xl leading-[1.2] font-bold max-sm:px-[var(--responsive-gutter-width)] max-sm:text-3xl sm:py-32 xl:text-center">
+                Over the past few years, I&apos;ve worked <br /> with a wide
+                range of technologies
+              </p>
+            </DisplayMsg>
+            <DisplayMsg className="line2 absolute translate-x-8 opacity-0 blur-lg max-sm:px-[var(--responsive-gutter-width)] lg:top-1/4 lg:max-w-96">
+              <p className="text-text bg-linear-to-t from-transparent via-cyan-50 to-transparent text-5xl leading-[1.2] max-sm:text-3xl sm:max-lg:py-32 sm:max-lg:text-center lg:text-4xl">
+                <span className="text-highlight">
+                  Some of them truly stuck with me,
+                </span>{' '}
+                so those are the ones I&apos;m taking along.
+              </p>
+            </DisplayMsg>
+          </div>
+          <div className="wheels sm:absolute-center relative col-span-2 row-span-2 w-[min(100%,43rem)] overflow-hidden max-sm:-mt-16">
+            <SmallWheelLogos />
+            <BigWheelLogos />
+          </div>
         </div>
       </div>
       <StatsSubSection />
